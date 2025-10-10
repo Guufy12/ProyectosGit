2851d41458a036ad36e35802dd85f327bd28455d
Carlos Perez <carloseuris@gmail.com>
2025-10-10 18:51:14 -0400

    Ejercicio 14 y 15 completado

---
+++ T2 - Practicando con formularios/Form8.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace T2___Practicando_con_formularios
+{
+    partial class Form8
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            label1 = new Label();
+            tabPage2 = new TabPage();
+            label2 = new Label();
+            tabPage3 = new TabPage();
+            label3 = new Label();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            saveFileDialog1 = new SaveFileDialog();
+            label4 = new Label();
+            button3 = new Button();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            tabPage3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Controls.Add(tabPage3);
+            tabControl1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            tabControl1.Location = new Point(32, 34);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(328, 315);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.BackColor = Color.Gray;
+            tabPage1.Controls.Add(label1);
+            tabPage1.Location = new Point(4, 30);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(320, 281);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.FromArgb(224, 224, 224);
+            label1.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(24, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(193, 22);
+            label1.TabIndex = 0;
+            label1.Text = "PRIMERA PESTAÑA";
+            // 
+            // tabPage2
+            // 
+            tabPage2.BackColor = Color.RosyBrown;
+            tabPage2.Controls.Add(label2);
+            tabPage2.Location = new Point(4, 30);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(320, 281);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Monotype Corsiva", 14.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label2.Location = new Point(22, 40);
+            label2.Name = "label2";
+            label2.Size = new Size(192, 22);
+            label2.TabIndex = 1;
+            label2.Text = "SEGUNDA PESTAÑA";
+            // 
+            // tabPage3
+            // 
+            tabPage3.BackColor = SystemColors.ActiveCaption;
+            tabPage3.Controls.Add(label3);
+            tabPage3.Location = new Point(4, 30);
+            tabPage3.Name = "tabPage3";
+            tabPage3.Padding = new Padding(3);
+            tabPage3.Size = new Size(320, 281);
+            tabPage3.TabIndex = 2;
+            tabPage3.Text = "tabPage3";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Tempus Sans ITC", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(17, 29);
+            label3.Name = "label3";
+            label3.Size = new Size(192, 24);
+            label3.TabIndex = 1;
+            label3.Text = "TERCERA PESTAÑA";
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.Location = new Point(541, 121);
+            button2.Name = "button2";
+            button2.Size = new Size(106, 51);
+            button2.TabIndex = 3;
+            button2.Text = "GUARDAR ARCHIVO";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(412, 64);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(336, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(412, 121);
+            button1.Name = "button1";
+            button1.Size = new Size(106, 51);
+            button1.TabIndex = 1;
+            button1.Text = "BUSCAR ARCHIVO";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "ArchivoTxt";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.Location = new Point(412, 34);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 4;
+            label4.Text = "RUTA";
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.Location = new Point(653, 121);
+            button3.Name = "button3";
+            button3.Size = new Size(106, 51);
+            button3.TabIndex = 5;
+            button3.Text = "MOSTRAR";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form8
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Tan;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(label4);
+            Controls.Add(button2);
+            Controls.Add(tabControl1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form8";
+            Text = "Form8";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            tabPage2.ResumeLayout(false);
+            tabPage2.PerformLayout();
+            tabPage3.ResumeLayout(false);
+            tabPage3.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private Label label1;
+        private TabPage tabPage2;
+        private Label label2;
+        private TabPage tabPage3;
+        private Label label3;
+        private OpenFileDialog openFileDialog1;
+        private Button button2;
+        private TextBox textBox1;
+        private Button button1;
+        private SaveFileDialog saveFileDialog1;
+        private Label label4;
+        private Button button3;
+    }
+}
---
+++ T2 - Practicando con formularios/Form8.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T2___Practicando_con_formularios
+{
+    public partial class Form8 : Form
+    {
+        public Form8()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                label4.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string nuevoArchivo = saveFileDialog1.FileName;
+                saveFileDialog1.Title = "GuardadoTxt";
+                string contenido = "Hola, soy el texto dentro del documento";
+                File.WriteAllText(nuevoArchivo,contenido);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string ruta = label4.Text;
+            //StreamReader lector = new StreamReader(ruta);
+            string contenido = File.ReadAllText(ruta);
+            textBox1.Text = contenido;
+        }
+    }
+}
