07231bf95169df823818c9b212e01aefa82223c1
Carlos Perez <carloseuris@gmail.com>
2025-10-10 11:49:02 -0400

    Ejercicios 9, 10 y 11 completados

---
+++ T2 - Practicando con formularios/Form5.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace T2___Practicando_con_formularios
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            label2 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            button3 = new Button();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(71, 151);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "VALIDAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(39, 44);
+            label1.Name = "label1";
+            label1.Size = new Size(165, 19);
+            label1.TabIndex = 1;
+            label1.Text = "DIGITE EL NUMERO";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(58, 87);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 2;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(249, 87);
+            button2.Name = "button2";
+            button2.Size = new Size(136, 39);
+            button2.TabIndex = 3;
+            button2.Text = "Boton mensaje";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(249, 44);
+            label2.Name = "label2";
+            label2.Size = new Size(136, 19);
+            label2.TabIndex = 4;
+            label2.Text = "TOCA EL BOTON";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(474, 93);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 5;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(488, 169);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 6;
+            button3.Text = "MOSTRAR";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Nirmala UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(488, 131);
+            label3.Name = "label3";
+            label3.Size = new Size(64, 21);
+            label3.TabIndex = 7;
+            label3.Text = " FECHA";
+            // 
+            // Form5
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(255, 255, 192);
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(button3);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(label2);
+            Controls.Add(button2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form5";
+            Text = "Form5";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private Button button2;
+        private Label label2;
+        private DateTimePicker dateTimePicker1;
+        private Button button3;
+        private Label label3;
+    }
+}
---
+++ T2 - Practicando con formularios/Form5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T2___Practicando_con_formularios
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int valor = Convert.ToInt32(textBox1.Text);
+                MessageBox.Show($"Valor ingresado: {valor}, es un numero");
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show("El valor no es un numero, Error: " + ex);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola, soy Carlos Perez de matricula: SD-2023-04835");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label3.Text = dateTimePicker1.Value.ToString(); 
+        }
+    }
+}
