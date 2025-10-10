d7673da84ddc5a734c940ad16aa972ec2ab00375
Carlos Perez <carloseuris@gmail.com>
2025-10-10 10:24:24 -0400

    Ejercicios 5 y 6 completados

---
+++ T2 - Practicando con formularios/Form3.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace T2___Practicando_con_formularios
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            groupBox1 = new GroupBox();
+            button2 = new Button();
+            label3 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.BackColor = Color.White;
+            radioButton1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton1.Location = new Point(70, 43);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(117, 25);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "ESTUDIANTE";
+            radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.BackColor = Color.FromArgb(224, 224, 224);
+            radioButton2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton2.Location = new Point(70, 89);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(99, 25);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "MAESTRO";
+            radioButton2.UseVisualStyleBackColor = false;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.BackColor = Color.Gray;
+            radioButton3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton3.Location = new Point(69, 136);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(100, 25);
+            radioButton3.TabIndex = 2;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "DIRECTOR";
+            radioButton3.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Black;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(170, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(345, 25);
+            label1.TabIndex = 3;
+            label1.Text = "ELIJA LA OPCION A LA QUE PERTENECE";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.FromArgb(0, 0, 64);
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(134, 263);
+            label2.Name = "label2";
+            label2.Size = new Size(97, 21);
+            label2.TabIndex = 4;
+            label2.Text = "SU OPCION";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(117, 305);
+            button1.Name = "button1";
+            button1.Size = new Size(117, 40);
+            button1.TabIndex = 5;
+            button1.Text = "ACEPTAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Rojo", "Verde", "Azul" });
+            comboBox1.Location = new Point(491, 128);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.FromArgb(192, 255, 255);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton3);
+            groupBox1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox1.ForeColor = SystemColors.ActiveCaptionText;
+            groupBox1.Location = new Point(46, 52);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(318, 197);
+            groupBox1.TabIndex = 7;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Opciones";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(509, 239);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 8;
+            button2.Text = "CAMBIAR";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.FromArgb(0, 0, 64);
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(491, 95);
+            label3.Name = "label3";
+            label3.Size = new Size(80, 21);
+            label3.TabIndex = 9;
+            label3.Text = "COLORES";
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(128, 255, 255);
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(button2);
+            Controls.Add(groupBox1);
+            Controls.Add(comboBox1);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form3";
+            Text = "Form3";
+            Load += Form3_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private RadioButton radioButton3;
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private ComboBox comboBox1;
+        private GroupBox groupBox1;
+        private Button button2;
+        private Label label3;
+    }
+}
---
+++ T2 - Practicando con formularios/Form3.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T2___Practicando_con_formularios
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked == true)
+            {
+                label2.Text = radioButton1.Text;
+            }
+            if (radioButton2.Checked == true)
+            {
+                label2.Text = radioButton2.Text;
+            }
+            if (radioButton3.Checked == true)
+            {
+                label2.Text = radioButton3.Text;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "Azul")
+            {
+                BackColor  = Color.Blue;
+            }
+            if (comboBox1.Text == "Verde")
+            {
+                BackColor = Color.Green;
+            }
+            if (comboBox1.Text == "Rojo")
+            {
+                BackColor = Color.Red;
+            }
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
