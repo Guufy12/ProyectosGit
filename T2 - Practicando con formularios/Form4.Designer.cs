84251dcfd0a1557e231fde1d18fac1cd3a936556
Carlos Perez <carloseuris@gmail.com>
2025-10-10 10:44:29 -0400

    Ejercicio 7 completado

---
+++ T2 - Practicando con formularios/Form4.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace T2___Practicando_con_formularios
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Universidades = new ListBox();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // Universidades
+            // 
+            Universidades.FormattingEnabled = true;
+            Universidades.ItemHeight = 15;
+            Universidades.Location = new Point(293, 44);
+            Universidades.Name = "Universidades";
+            Universidades.Size = new Size(120, 94);
+            Universidades.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(72, 55);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(107, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(81, 97);
+            button1.Name = "button1";
+            button1.Size = new Size(87, 41);
+            button1.TabIndex = 2;
+            button1.Text = "AGREGAR";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(59, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(209, 21);
+            label1.TabIndex = 3;
+            label1.Text = "AGREGA LA UNIVERSIDAD";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(305, 179);
+            button2.Name = "button2";
+            button2.Size = new Size(87, 41);
+            button2.TabIndex = 4;
+            button2.Text = "LIMPIAR";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form4
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DeepSkyBlue;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(Universidades);
+            Name = "Form4";
+            Text = "Form4";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListBox Universidades;
+        private TextBox textBox1;
+        private Button button1;
+        private Label label1;
+        private Button button2;
+    }
+}
---
+++ T2 - Practicando con formularios/Form4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T2___Practicando_con_formularios
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Universidades.Items.Add(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Universidades.Items.Clear();
+        }
+    }
+}
