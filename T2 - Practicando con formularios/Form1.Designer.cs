c9bc60ecb2e12d202885df024546bba09e38de07
Carlos Perez <carloseuris@gmail.com>
2025-10-10 09:13:24 -0400

    Inicio de formulario label y button

---
+++ T2 - Practicando con formularios/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace T2___Practicando_con_formularios
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            btnLabel = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.Control;
+            label1.Font = new Font("Arial Narrow", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(169, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(431, 20);
+            label1.TabIndex = 0;
+            label1.Text = "¡BIENVENIDOS AL FORMULARIO DE CARLOS PEREZ SD-2023-04835!";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.AliceBlue;
+            label2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(50, 75);
+            label2.Name = "label2";
+            label2.Size = new Size(125, 25);
+            label2.TabIndex = 1;
+            label2.Text = "BLACK LABEL";
+            // 
+            // btnLabel
+            // 
+            btnLabel.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLabel.Location = new Point(50, 142);
+            btnLabel.Name = "btnLabel";
+            btnLabel.Size = new Size(125, 37);
+            btnLabel.TabIndex = 2;
+            btnLabel.Text = "CAMBIAR TEXTO";
+            btnLabel.UseVisualStyleBackColor = true;
+            btnLabel.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Blue;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnLabel);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button btnLabel;
+    }
+}
