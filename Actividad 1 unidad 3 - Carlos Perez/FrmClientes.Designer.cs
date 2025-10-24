a7b45d61cfbb73ee9ebda982c9dff642fa59e788
Carlos Perez <carloseuris@gmail.com>
2025-10-24 15:51:13 -0400

    Crud clientes estructurado

--- Actividad 1 unidad 3 - Carlos Perez/FrmClientes.Designer.cs
+++ Actividad 1 unidad 3 - Carlos Perez/FrmClientes.Designer.cs
@@ -29,20 +29,16 @@
         private void InitializeComponent()
         {
             groupBox2 = new GroupBox();
-            groupBox3 = new GroupBox();
-            groupBox4 = new GroupBox();
-            lblClienteID = new Label();
-            txtClienteID = new TextBox();
-            groupBox1 = new GroupBox();
-            txtDireccion = new TextBox();
-            txtTelefono = new TextBox();
-            txtCorreo = new TextBox();
-            txtNombre = new TextBox();
-            label4 = new Label();
-            label3 = new Label();
-            label2 = new Label();
-            label1 = new Label();
+            btnMostrar = new Button();
             dgvClientes = new DataGridView();
+            groupBox3 = new GroupBox();
+            btnEliminar = new Button();
+            txtEliminarCliente = new TextBox();
+            label10 = new Label();
+            groupBox4 = new GroupBox();
+            txtActualizarClienteid = new TextBox();
+            label9 = new Label();
+            btnActualizar = new Button();
             txtActualizarDireccion = new TextBox();
             txtActualizarTelefono = new TextBox();
             TxtActualizarCorreo = new TextBox();
@@ -51,19 +47,23 @@
             label6 = new Label();
             label7 = new Label();
             label8 = new Label();
-            txtActualizarCliente = new TextBox();
-            label9 = new Label();
-            txtEliminarCliente = new TextBox();
-            label10 = new Label();
+            lblClienteID = new Label();
+            txtClienteID = new TextBox();
+            groupBox1 = new GroupBox();
             btnAgregar = new Button();
-            btnMostrar = new Button();
-            btnEliminar = new Button();
-            btnActualizar = new Button();
+            txtDireccion = new TextBox();
+            txtTelefono = new TextBox();
+            txtCorreo = new TextBox();
+            txtNombre = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
             groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvClientes).BeginInit();
             groupBox3.SuspendLayout();
             groupBox4.SuspendLayout();
             groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)dgvClientes).BeginInit();
             SuspendLayout();
             // 
             // groupBox2
@@ -71,12 +71,31 @@
             groupBox2.BackColor = Color.White;
             groupBox2.Controls.Add(btnMostrar);
             groupBox2.Controls.Add(dgvClientes);
-            groupBox2.Location = new Point(423, 2);
+            groupBox2.Location = new Point(433, 12);
             groupBox2.Name = "groupBox2";
             groupBox2.Size = new Size(450, 236);
             groupBox2.TabIndex = 2;
             groupBox2.TabStop = false;
             groupBox2.Text = "LISTA CLIENTES";
+            // 
+            // btnMostrar
+            // 
+            btnMostrar.Location = new Point(278, 196);
+            btnMostrar.Name = "btnMostrar";
+            btnMostrar.Size = new Size(106, 34);
+            btnMostrar.TabIndex = 15;
+            btnMostrar.Text = "MOSTRAR";
+            btnMostrar.UseVisualStyleBackColor = true;
+            btnMostrar.Click += btnMostrar_Click;
+            // 
+            // dgvClientes
+            // 
+            dgvClientes.BackgroundColor = SystemColors.Control;
+            dgvClientes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvClientes.Location = new Point(17, 31);
+            dgvClientes.Name = "dgvClientes";
+            dgvClientes.Size = new Size(412, 159);
+            dgvClientes.TabIndex = 0;
             // 
             // groupBox3
             // 
@@ -85,16 +104,45 @@
             groupBox3.Controls.Add(txtEliminarCliente);
             groupBox3.Controls.Add(label10);
             groupBox3.Font = new Font("Times New Roman", 11.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            groupBox3.Location = new Point(531, 285);
+            groupBox3.Location = new Point(477, 302);
             groupBox3.Name = "groupBox3";
             groupBox3.Size = new Size(306, 107);
             groupBox3.TabIndex = 3;
             groupBox3.TabStop = false;
             groupBox3.Text = "ELIMINAR CLIENTE";
             // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(147, 67);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(106, 34);
+            btnEliminar.TabIndex = 16;
+            btnEliminar.Text = "ELIMINAR";
+            btnEliminar.UseVisualStyleBackColor = true;
+            btnEliminar.Click += btnEliminar_Click;
+            // 
+            // txtEliminarCliente
+            // 
+            txtEliminarCliente.Location = new Point(133, 31);
+            txtEliminarCliente.Name = "txtEliminarCliente";
+            txtEliminarCliente.Size = new Size(143, 25);
+            txtEliminarCliente.TabIndex = 7;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Times New Roman", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.Location = new Point(30, 35);
+            label10.Name = "label10";
+            label10.Size = new Size(86, 17);
+            label10.TabIndex = 6;
+            label10.Text = "CLIENTEID";
+            // 
             // groupBox4
             // 
             groupBox4.BackColor = SystemColors.ButtonFace;
+            groupBox4.Controls.Add(txtActualizarClienteid);
+            groupBox4.Controls.Add(label9);
             groupBox4.Controls.Add(btnActualizar);
             groupBox4.Controls.Add(txtActualizarDireccion);
             groupBox4.Controls.Add(txtActualizarTelefono);
@@ -104,15 +152,103 @@
             groupBox4.Controls.Add(label6);
             groupBox4.Controls.Add(label7);
             groupBox4.Controls.Add(label8);
-            groupBox4.Controls.Add(txtActualizarCliente);
-            groupBox4.Controls.Add(label9);
             groupBox4.Font = new Font("Times New Roman", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            groupBox4.Location = new Point(12, 254);
+            groupBox4.Location = new Point(12, 266);
             groupBox4.Name = "groupBox4";
             groupBox4.Size = new Size(426, 234);
             groupBox4.TabIndex = 4;
             groupBox4.TabStop = false;
             groupBox4.Text = "ACTUALIZAR DATOS";
+            // 
+            // txtActualizarClienteid
+            // 
+            txtActualizarClienteid.Location = new Point(156, 54);
+            txtActualizarClienteid.Name = "txtActualizarClienteid";
+            txtActualizarClienteid.Size = new Size(143, 22);
+            txtActualizarClienteid.TabIndex = 26;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(41, 57);
+            label9.Name = "label9";
+            label9.Size = new Size(70, 15);
+            label9.TabIndex = 25;
+            label9.Text = "CLIENTEID";
+            // 
+            // btnActualizar
+            // 
+            btnActualizar.Location = new Point(194, 194);
+            btnActualizar.Name = "btnActualizar";
+            btnActualizar.Size = new Size(106, 34);
+            btnActualizar.TabIndex = 24;
+            btnActualizar.Text = "ACTUALIZAR";
+            btnActualizar.UseVisualStyleBackColor = true;
+            btnActualizar.Click += btnActualizar_Click;
+            // 
+            // txtActualizarDireccion
+            // 
+            txtActualizarDireccion.Location = new Point(194, 155);
+            txtActualizarDireccion.Name = "txtActualizarDireccion";
+            txtActualizarDireccion.Size = new Size(217, 22);
+            txtActualizarDireccion.TabIndex = 23;
+            // 
+            // txtActualizarTelefono
+            // 
+            txtActualizarTelefono.Location = new Point(156, 93);
+            txtActualizarTelefono.Name = "txtActualizarTelefono";
+            txtActualizarTelefono.Size = new Size(143, 22);
+            txtActualizarTelefono.TabIndex = 22;
+            // 
+            // TxtActualizarCorreo
+            // 
+            TxtActualizarCorreo.Location = new Point(194, 122);
+            TxtActualizarCorreo.Name = "TxtActualizarCorreo";
+            TxtActualizarCorreo.Size = new Size(203, 22);
+            TxtActualizarCorreo.TabIndex = 21;
+            // 
+            // txtActualizarNombre
+            // 
+            txtActualizarNombre.Location = new Point(184, 22);
+            txtActualizarNombre.Name = "txtActualizarNombre";
+            txtActualizarNombre.Size = new Size(204, 22);
+            txtActualizarNombre.TabIndex = 20;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(38, 158);
+            label5.Name = "label5";
+            label5.Size = new Size(73, 15);
+            label5.TabIndex = 19;
+            label5.Text = "DIRECCION";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(38, 96);
+            label6.Name = "label6";
+            label6.Size = new Size(70, 15);
+            label6.TabIndex = 18;
+            label6.Text = "TELEFONO";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(23, 129);
+            label7.Name = "label7";
+            label7.Size = new Size(143, 15);
+            label7.TabIndex = 17;
+            label7.Text = "CORREO ELECTRONICO";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(36, 25);
+            label8.Name = "label8";
+            label8.Size = new Size(130, 15);
+            label8.TabIndex = 16;
+            label8.Text = "NOMBRE COMPLETO";
             // 
             // lblClienteID
             // 
@@ -151,6 +287,16 @@
             groupBox1.TabIndex = 1;
             groupBox1.TabStop = false;
             groupBox1.Text = "DATOS CLIENTE";
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(185, 186);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(106, 34);
+            btnAgregar.TabIndex = 14;
+            btnAgregar.Text = "AGREGAR";
+            btnAgregar.UseVisualStyleBackColor = true;
+            btnAgregar.Click += btnAgregar_Click;
             // 
             // txtDireccion
             // 
@@ -217,148 +363,6 @@
             label1.TabIndex = 6;
             label1.Text = "NOMBRE COMPLETO";
             // 
-            // dgvClientes
-            // 
-            dgvClientes.BackgroundColor = SystemColors.Control;
-            dgvClientes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvClientes.Location = new Point(17, 31);
-            dgvClientes.Name = "dgvClientes";
-            dgvClientes.Size = new Size(412, 159);
-            dgvClientes.TabIndex = 0;
-            // 
-            // txtActualizarDireccion
-            // 
-            txtActualizarDireccion.Location = new Point(185, 154);
-            txtActualizarDireccion.Name = "txtActualizarDireccion";
-            txtActualizarDireccion.Size = new Size(217, 22);
-            txtActualizarDireccion.TabIndex = 23;
-            // 
-            // txtActualizarTelefono
-            // 
-            txtActualizarTelefono.Location = new Point(147, 92);
-            txtActualizarTelefono.Name = "txtActualizarTelefono";
-            txtActualizarTelefono.Size = new Size(143, 22);
-            txtActualizarTelefono.TabIndex = 22;
-            // 
-            // TxtActualizarCorreo
-            // 
-            TxtActualizarCorreo.Location = new Point(185, 121);
-            TxtActualizarCorreo.Name = "TxtActualizarCorreo";
-            TxtActualizarCorreo.Size = new Size(203, 22);
-            TxtActualizarCorreo.TabIndex = 21;
-            // 
-            // txtActualizarNombre
-            // 
-            txtActualizarNombre.Location = new Point(184, 22);
-            txtActualizarNombre.Name = "txtActualizarNombre";
-            txtActualizarNombre.Size = new Size(204, 22);
-            txtActualizarNombre.TabIndex = 20;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(40, 157);
-            label5.Name = "label5";
-            label5.Size = new Size(73, 15);
-            label5.TabIndex = 19;
-            label5.Text = "DIRECCION";
-            // 
-            // label6
-            // 
-            label6.AutoSize = true;
-            label6.Location = new Point(40, 95);
-            label6.Name = "label6";
-            label6.Size = new Size(70, 15);
-            label6.TabIndex = 18;
-            label6.Text = "TELEFONO";
-            // 
-            // label7
-            // 
-            label7.AutoSize = true;
-            label7.Location = new Point(25, 128);
-            label7.Name = "label7";
-            label7.Size = new Size(143, 15);
-            label7.TabIndex = 17;
-            label7.Text = "CORREO ELECTRONICO";
-            // 
-            // label8
-            // 
-            label8.AutoSize = true;
-            label8.Location = new Point(36, 25);
-            label8.Name = "label8";
-            label8.Size = new Size(130, 15);
-            label8.TabIndex = 16;
-            label8.Text = "NOMBRE COMPLETO";
-            // 
-            // txtActualizarCliente
-            // 
-            txtActualizarCliente.Location = new Point(147, 54);
-            txtActualizarCliente.Name = "txtActualizarCliente";
-            txtActualizarCliente.Size = new Size(84, 22);
-            txtActualizarCliente.TabIndex = 15;
-            // 
-            // label9
-            // 
-            label9.AutoSize = true;
-            label9.Location = new Point(40, 62);
-            label9.Name = "label9";
-            label9.Size = new Size(70, 15);
-            label9.TabIndex = 14;
-            label9.Text = "CLIENTEID";
-            // 
-            // txtEliminarCliente
-            // 
-            txtEliminarCliente.Location = new Point(133, 31);
-            txtEliminarCliente.Name = "txtEliminarCliente";
-            txtEliminarCliente.Size = new Size(143, 25);
-            txtEliminarCliente.TabIndex = 7;
-            // 
-            // label10
-            // 
-            label10.AutoSize = true;
-            label10.Font = new Font("Times New Roman", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label10.Location = new Point(30, 35);
-            label10.Name = "label10";
-            label10.Size = new Size(86, 17);
-            label10.TabIndex = 6;
-            label10.Text = "CLIENTEID";
-            // 
-            // btnAgregar
-            // 
-            btnAgregar.Location = new Point(185, 186);
-            btnAgregar.Name = "btnAgregar";
-            btnAgregar.Size = new Size(106, 34);
-            btnAgregar.TabIndex = 14;
-            btnAgregar.Text = "AGREGAR";
-            btnAgregar.UseVisualStyleBackColor = true;
-            // 
-            // btnMostrar
-            // 
-            btnMostrar.Location = new Point(278, 196);
-            btnMostrar.Name = "btnMostrar";
-            btnMostrar.Size = new Size(106, 34);
-            btnMostrar.TabIndex = 15;
-            btnMostrar.Text = "MOSTRAR";
-            btnMostrar.UseVisualStyleBackColor = true;
-            // 
-            // btnEliminar
-            // 
-            btnEliminar.Location = new Point(147, 67);
-            btnEliminar.Name = "btnEliminar";
-            btnEliminar.Size = new Size(106, 34);
-            btnEliminar.TabIndex = 16;
-            btnEliminar.Text = "ELIMINAR";
-            btnEliminar.UseVisualStyleBackColor = true;
-            // 
-            // btnActualizar
-            // 
-            btnActualizar.Location = new Point(194, 194);
-            btnActualizar.Name = "btnActualizar";
-            btnActualizar.Size = new Size(106, 34);
-            btnActualizar.TabIndex = 24;
-            btnActualizar.Text = "ACTUALIZAR";
-            btnActualizar.UseVisualStyleBackColor = true;
-            // 
             // FrmClientes
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -372,13 +376,13 @@
             Name = "FrmClientes";
             Text = "FORMULARIO CLIENTES";
             groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dgvClientes).EndInit();
             groupBox3.ResumeLayout(false);
             groupBox3.PerformLayout();
             groupBox4.ResumeLayout(false);
             groupBox4.PerformLayout();
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)dgvClientes).EndInit();
             ResumeLayout(false);
         }
 
@@ -406,13 +410,13 @@
         private Label label6;
         private Label label7;
         private Label label8;
-        private TextBox txtActualizarCliente;
-        private Label label9;
         private Button btnMostrar;
         private Button btnEliminar;
         private TextBox txtEliminarCliente;
         private Label label10;
         private Button btnActualizar;
         private Button btnAgregar;
+        private TextBox txtActualizarClienteid;
+        private Label label9;
     }
 }
--- Actividad 1 unidad 3 - Carlos Perez/FrmClientes.cs
+++ Actividad 1 unidad 3 - Carlos Perez/FrmClientes.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
 
 namespace Actividad_1_unidad_3___Carlos_Perez
 {
@@ -21,5 +22,127 @@ namespace Actividad_1_unidad_3___Carlos_Perez
         {
 
         }
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionStr = "Data source = localhost; initial catalog= TiendaDB; Integrated security=True;TrustServerCertificate=True";
+                using (SqlConnection conexion = new SqlConnection(connectionStr))
+                {
+                    conexion.Open();
+                    int clienteId = Convert.ToInt32(txtClienteID.Text);
+                    string comandoInsertar = @$"Insert into Clientes(Cliente_ID, NOMBRE_COMPLETO, CORREO_ELECTRONICO,TELEFONO, DIRECCION)
+                                           VALUES({clienteId}, '{txtNombre.Text}', '{txtCorreo.Text}','{txtTelefono.Text}', '{txtDireccion.Text}')";
+
+
+                    using (SqlCommand comando = new SqlCommand(comandoInsertar, conexion))
+                    {
+                        int celdasAfectadas = comando.ExecuteNonQuery();
+                        if (celdasAfectadas > 0)
+                        {
+                            MessageBox.Show("Cliente Agregado");
+
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+            }
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionStr = "Data source = localhost; initial catalog= TiendaDB; Integrated security=True;TrustServerCertificate=True";
+                using (SqlConnection conexion = new SqlConnection(connectionStr))
+                {
+                    conexion.Open();
+                    string comandoMostrar = "Select * From Clientes";
+
+                    using (SqlCommand comando = new SqlCommand(comandoMostrar, conexion))
+                    {
+                        using (SqlDataAdapter adapter = new SqlDataAdapter(comando))
+                        {
+                            DataTable Tabla = new DataTable();
+                            adapter.Fill(Tabla);
+                            dgvClientes.DataSource = Tabla;
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionStr = "Data source = localhost; initial catalog= TiendaDB; Integrated security=True;TrustServerCertificate=True";
+                using (SqlConnection conexion = new SqlConnection(connectionStr))
+                {
+                    conexion.Open();
+                    int clienteId = Convert.ToInt32(txtActualizarClienteid.Text);
+                    string comandoActualizar = @$"Update Clientes Set NOMBRE_COMPLETO  = '{txtActualizarNombre.Text}', CORREO_ELECTRONICO ='{TxtActualizarCorreo.Text}' ,TELEFONO = '{txtActualizarTelefono.Text}', DIRECCION= '{txtActualizarDireccion.Text}'
+                                                  Where Cliente_id = {clienteId}";
+
+                    using (SqlCommand comando = new SqlCommand(comandoActualizar, conexion))
+                    {
+                        int celdasAfectadas = comando.ExecuteNonQuery();
+                        if (celdasAfectadas > 0)
+                        {
+                            MessageBox.Show("Cliente Actualizado");
+
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+            }
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+ 
+            try
+            {
+                string connectionStr = "Data source = localhost; initial catalog= TiendaDB; Integrated security=True;TrustServerCertificate=True";
+                using (SqlConnection conexion = new SqlConnection(connectionStr))
+                {
+                    conexion.Open();
+                    int clienteId = Convert.ToInt32(txtEliminarCliente.Text);
+                    string comandoMostrar = $"Delete from Clientes where Cliente_ID = {clienteId}";
+
+                    using (SqlCommand comando = new SqlCommand(comandoMostrar, conexion))
+                    {
+                        int celdasAfectadas = comando.ExecuteNonQuery();
+                        if (celdasAfectadas > 0)
+                        {
+                            MessageBox.Show("Cliente Eliminado");
+
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex);
+            }
+        }
     }
+    
+    
 }
