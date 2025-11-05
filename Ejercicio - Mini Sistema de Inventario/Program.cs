e82f7efcb739f3c3948b5687d853588761eec7d5
Carlos Perez <carloseuris@gmail.com>
2025-11-05 19:29:39 -0400

    Inicio

---
+++ Ejercicio - Mini Sistema de Inventario/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Ya funciono...");
+    }
+}
+
+class Producto
+{
+    int Id;
+    string Nombre;
+    decimal Precio;
+    int Cantidad;
+
+    public Producto(int id, string nombre, decimal precio, int cantidad)
+    {
+        Id = id;
+        Nombre = nombre;
+        Precio = precio;
+        Cantidad = cantidad;
+    }
+
+    public void MostrarInfo()
+    {
+        Console.WriteLine($"Datos del producto: Id: {Id}, Nombre: {Nombre}, Precio: {Precio}, Cantidad: {Cantidad}");
+    }
+
+     interface IGestion<T>
+    {
+        void Agregar(T item);
+        void Eliminar(int id);
+        T BuscarId(int id);
+        void Listar();
+    }
+
+    class Inventario : IGestion<Producto>
+    {
+
+    }
+}
+
