d9623776c1009f2d5a404b9fbde0a7654401a9d9
Carlos Perez <carloseuris@gmail.com>
2025-11-21 11:51:22 -0400

    Creacion proyecto

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Carro carro = new Carro("ferrari", 300);
+
+        carro.Estado();
+    }
+}
+class Carro
+{
+    string Marca { get; set; }
+    int Velocidad { get; set; }
+
+    public Carro(string marca, int velocidad)
+    {
+        Marca = marca;
+        Velocidad = velocidad;
+    }
+
+    public void Estado()
+    {
+        Console.WriteLine("El carro: " + Marca + " esta encendido y corre a: " + Velocidad);
+    }
+}
---
+++ Proyecto final - Sistema de calificacion estudiantes/Form1.cs
@@ -0,0 +1,10 @@
+namespace Proyecto_final___Sistema_de_calificacion_estudiantes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
