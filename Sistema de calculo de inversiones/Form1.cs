60e96ad9efbdb3fa8776e7ae854b2b5c13f8f74e
CARLOS PEREZ <carloseuris@gmail.com>
2025-12-10 14:50:20 -0400

    Proyecto calculo de inversiones

---
+++ Sistema de calculo de inversiones/Form1.cs
@@ -0,0 +1,10 @@
+namespace Sistema_de_calculo_de_inversiones
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
